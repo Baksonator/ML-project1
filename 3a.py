635a50577c8718faa7629ba5bd4e8d9fd0c27697
antic <santic16@raf.rs>
2019-04-24 01:00:28 +0200

    Finished 3b and 3c

--- 3a.py
+++ 3a.py
@@ -4,6 +4,8 @@ import sys
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+
+
 # np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -64,84 +66,86 @@ class KNN:
             return accuracy, predicted
 
 
-filename = 'data/iris.csv'
-mapper_key_val = {
-    0: 'Iris-setosa', 1: 'Iris-versicolor', 2: 'Iris-virginica'
-}
-mapper_val_key = {v: k for k, v in mapper_key_val.items()}
+if __name__ == '__main':
+    filename = 'data/iris.csv'
+    mapper_key_val = {
+        0: 'Iris-setosa', 1: 'Iris-versicolor', 2: 'Iris-virginica'
+    }
+    mapper_val_key = {v: k for k, v in mapper_key_val.items()}
 
-all_data = np.loadtxt(filename, delimiter=',', dtype=str, skiprows=1)
-all_data = np.array([np.array([item[0], item[1], item[2], item[3], mapper_val_key[item[4]]]).astype(float) for item in all_data])
+    all_data = np.loadtxt(filename, delimiter=',', dtype=str, skiprows=1)
+    all_data = np.array(
+        [np.array([item[0], item[1], item[2], item[3], mapper_val_key[item[4]]]).astype(float) for item in all_data])
 
-nb_features = 2 # prva dva featurea
-nb_classes = 3 # 3 vrste cveta perunike
-data = dict()
-data['x'] = all_data[:, :nb_features]
-data['y'] = all_data[:, 4]
+    nb_features = 2  # prva dva featurea
+    nb_classes = 3  # 3 vrste cveta perunike
+    data = dict()
+    data['x'] = all_data[:, :nb_features]
+    data['y'] = all_data[:, 4]
 
-# Mesanje.
-nb_samples = data['x'].shape[0]
-indices = np.random.permutation(nb_samples)
-data['x'] = data['x'][indices]
-data['y'] = data['y'][indices]
+    # Mesanje.
+    nb_samples = data['x'].shape[0]
+    indices = np.random.permutation(nb_samples)
+    data['x'] = data['x'][indices]
+    data['y'] = data['y'][indices]
 
-# Normalizacija
-data['x'] = (data['x'] - np.mean(data['x'], axis=0)) / np.std(data['x'], axis=0)
+    # Normalizacija
+    data['x'] = (data['x'] - np.mean(data['x'], axis=0)) / np.std(data['x'], axis=0)
 
-training_ratio = 0.8
-test_ratio = 0.2
+    training_ratio = 0.8
+    test_ratio = 0.2
 
-nb_train = int(training_ratio * nb_samples)
-data_train = dict()
-data_train['x'] = data['x'][:nb_train]
-data_train['y'] = data['y'][:nb_train]
+    nb_train = int(training_ratio * nb_samples)
+    data_train = dict()
+    data_train['x'] = data['x'][:nb_train]
+    data_train['y'] = data['y'][:nb_train]
 
-nb_test = nb_samples - nb_train
-data_test = dict()
-data_test['x'] = data['x'][nb_train:]
-data_test['y'] = data['y'][nb_train:]
+    nb_test = nb_samples - nb_train
+    data_test = dict()
+    data_test['x'] = data['x'][nb_train:]
+    data_test['y'] = data['y'][nb_train:]
 
-# Pokrecemo kNN na test skupu.
-k = 3
-train_data = {'x': data_train['x'], 'y': data_train['y']}
-knn = KNN(nb_features, nb_classes, train_data, k)
-accuracy, predicted = knn.predict({'x': data_test['x'], 'y': data_test['y']})
-print('Test set accuracy: ', accuracy)
+    # Pokrecemo kNN na test skupu.
+    k = 3
+    train_data = {'x': data_train['x'], 'y': data_train['y']}
+    knn = KNN(nb_features, nb_classes, train_data, k)
+    accuracy, predicted = knn.predict({'x': data_test['x'], 'y': data_test['y']})
+    print('Test set accuracy: ', accuracy)
 
-print('Building map...')
-# Generisemo grid.
-step_size = 0.01
-x1, x2 = np.meshgrid(np.arange(min(data['x'][:, 0]), max(data['x'][:, 0]),
-                               step_size),
-                     np.arange(min(data['x'][:, 1]), max(data['x'][:, 1]),
-                               step_size))
-x_feed = np.vstack((x1.flatten(), x2.flatten())).T
-# Racunamo vrednost hipoteze.
-accuracy, pred_val = knn.predict({'x': x_feed, 'y': None})
-# pred_val = sess.run(pred, feed_dict={X: x_feed})
+    print('Building map...')
+    # Generisemo grid.
+    step_size = 0.01
+    x1, x2 = np.meshgrid(np.arange(min(data['x'][:, 0]), max(data['x'][:, 0]),
+                                   step_size),
+                         np.arange(min(data['x'][:, 1]), max(data['x'][:, 1]),
+                                   step_size))
+    x_feed = np.vstack((x1.flatten(), x2.flatten())).T
+    # Racunamo vrednost hipoteze.
+    accuracy, pred_val = knn.predict({'x': x_feed, 'y': None})
+    # pred_val = sess.run(pred, feed_dict={X: x_feed})
 
-pred_val = np.array(pred_val)
-pred_plot = pred_val.reshape([x1.shape[0], x1.shape[1]])
+    pred_val = np.array(pred_val)
+    pred_plot = pred_val.reshape([x1.shape[0], x1.shape[1]])
 
-# Crtamo contour plot.
-from matplotlib.colors import LinearSegmentedColormap
+    # Crtamo contour plot.
+    from matplotlib.colors import LinearSegmentedColormap
 
-classes_cmap = LinearSegmentedColormap.from_list('classes_cmap',
-                                                 ['lightblue',
-                                                  'lightgreen',
-                                                  'lightyellow'])
-plt.contourf(x1, x2, pred_plot, cmap=classes_cmap, alpha=0.7)
+    classes_cmap = LinearSegmentedColormap.from_list('classes_cmap',
+                                                     ['lightblue',
+                                                      'lightgreen',
+                                                      'lightyellow'])
+    plt.contourf(x1, x2, pred_plot, cmap=classes_cmap, alpha=0.7)
 
-# Crtamo sve podatke preko.
-idxs_0 = train_data['y'] == 0.
-idxs_1 = train_data['y'] == 1.
-idxs_2 = train_data['y'] == 2.
-plt.scatter(train_data['x'][idxs_0, 0], train_data['x'][idxs_0, 1], c='b',
-            edgecolors='k', label=mapper_key_val[0])
-plt.scatter(train_data['x'][idxs_1, 0], train_data['x'][idxs_1, 1], c='g',
-            edgecolors='k', label=mapper_key_val[1])
-plt.scatter(train_data['x'][idxs_2, 0], train_data['x'][idxs_2, 1], c='y',
-            edgecolors='k', label=mapper_key_val[2])
-plt.legend()
+    # Crtamo sve podatke preko.
+    idxs_0 = train_data['y'] == 0.
+    idxs_1 = train_data['y'] == 1.
+    idxs_2 = train_data['y'] == 2.
+    plt.scatter(train_data['x'][idxs_0, 0], train_data['x'][idxs_0, 1], c='b',
+                edgecolors='k', label=mapper_key_val[0])
+    plt.scatter(train_data['x'][idxs_1, 0], train_data['x'][idxs_1, 1], c='g',
+                edgecolors='k', label=mapper_key_val[1])
+    plt.scatter(train_data['x'][idxs_2, 0], train_data['x'][idxs_2, 1], c='y',
+                edgecolors='k', label=mapper_key_val[2])
+    plt.legend()
 
-plt.show()
+    plt.show()
--- 3c.py
+++ 3c.py
@@ -1,1 +1,58 @@
 # TODO popuniti kodom za problem 3c
+# TODO popuniti kodom za problem 3b
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+_3a = __import__('3a')
+
+filename = 'data/iris.csv'
+mapper_key_val = {
+    0: 'Iris-setosa', 1: 'Iris-versicolor', 2: 'Iris-virginica'
+}
+mapper_val_key = {v: k for k, v in mapper_key_val.items()}
+
+all_data = np.loadtxt(filename, delimiter=',', dtype=str, skiprows=1)
+all_data = np.array([np.array([item[0], item[1], item[2], item[3], mapper_val_key[item[4]]]).astype(float) for item in all_data])
+
+nb_features = 4 # prva dva featurea
+nb_classes = 3 # 3 vrste cveta perunike
+data = dict()
+data['x'] = all_data[:, :nb_features]
+data['y'] = all_data[:, 4]
+
+# Mesanje.
+nb_samples = data['x'].shape[0]
+indices = np.random.permutation(nb_samples)
+data['x'] = data['x'][indices]
+data['y'] = data['y'][indices]
+
+# Normalizacija
+data['x'] = (data['x'] - np.mean(data['x'], axis=0)) / np.std(data['x'], axis=0)
+
+training_ratio = 0.8
+test_ratio = 0.2
+
+nb_train = int(training_ratio * nb_samples)
+data_train = dict()
+data_train['x'] = data['x'][:nb_train]
+data_train['y'] = data['y'][:nb_train]
+
+nb_test = nb_samples - nb_train
+data_test = dict()
+data_test['x'] = data['x'][nb_train:]
+data_test['y'] = data['y'][nb_train:]
+
+accuracies = list()
+for k in range(1, 16):
+    train_data = {'x': data_train['x'], 'y': data_train['y']}
+    knn = _3a.KNN(nb_features, nb_classes, train_data, k)
+    accuracy, predicted = knn.predict({'x': data_test['x'], 'y': data_test['y']})
+    print('Test set accuracy: ', accuracy)
+    accuracies.append(accuracy)
+
+print(accuracies)
+plt.plot([x for x in range(1, 16)], accuracies)
+plt.axis([0, 16, 0.5, 1])
+plt.grid(True)
+plt.show()
+
